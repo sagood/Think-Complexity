6ae8e64485708590c62ff03721de65449e62847b
tian <gasnus@gmail.com>
2015-11-12 20:08:47 +0100

    ex 2-6

--- Ch2/RandomGraph.py
+++ Ch2/RandomGraph.py
@@ -16,21 +16,40 @@ class RandomGraph(Graph):
                 self.add_edge(Edge(w, v))
 
 
-def main(script, n=10, p=0.2, *args):
-    n = int(n)
-    p = float(p)
-    labels = string.ascii_lowercase + string.ascii_uppercase
-    vs = [Vertex(c) for c in labels[:n]]
-
-    # create a graph and a layout
-    g = RandomGraph(vs)
-    g.add_random_edges(p)
+def show_graph(g):
     layout = CircleLayout(g)
 
     # draw the graph
     gw = GraphWorld()
     gw.show_graph(g, layout)
     gw.mainloop()
+
+
+def test_graph(n, p):
+    labels = string.ascii_lowercase + string.ascii_uppercase
+    vs = [Vertex(c) for c in labels[:n]]
+    g = RandomGraph(vs)
+    g.add_random_edges(p)
+    return g.is_connected()
+
+
+def test_p(n, p0, iteration):
+    p = p0
+    while p <= 1.0:
+        count = 0
+        for i in range(iteration):
+            if test_graph(n, p):
+                count += 1
+
+        print('p = {0:.2f}: {1:.2f}'.format(p, count / iteration))
+        p += 0.05
+
+
+def main(script, n=20, p=0.1, *args):
+    n = int(n)
+    p = float(p)
+    # ex 2-6
+    test_p(n, p, 1000)
 
 
 if __name__ == '__main__':
